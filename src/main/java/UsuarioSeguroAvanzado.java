6e8e40c3f7510adbcecebdcd9df97aec79acd821
Laboratorio <laboratorio@udla-ec.int>
2026-04-22 12:12:54 -0500

    Mateo Garzon vfinal

--- src/main/java/UsuarioSeguroAvanzado.java
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -7,89 +7,104 @@ public class UsuarioSeguroAvanzado {
     private int maxIntentos;
     private boolean accesoExitoso;
 
-    this.username = username;
-    this.password = password;
-    this.intentosFallidos = 0;
-    this.bloqueado = false;
-    this.accesoExitoso = false;
 
-    if (maxIntentos<=0) {
-        this.maxIntentos = 3;
-    }else {
-        this.maxIntentos = maxIntentos;
-
-    }
-
-
-}
-
-public String getUsername () {
-    return username;
-}
-public int getIntentosFallidos () {
-    return intentosFallidos;
-}
-public boolean isBloqueado () {
-    return bloqueado;
-}
-public int getMaxIntentos () {
-    return maxIntentos;
-}
-public boolean isAccesoExitoso () {
-    return accesoExitoso;
-}
-
-public boolean autenticar(String passwordIngresada) {
-    if (this.bloqueado) {
-        return false;
-    }
-    if (this.password.equals(passwordIngresada)) {
+    public UsuarioSeguroAvanzado(String username, String password, int maxIntentos) {
+        this.username = username;
+        this.password = password;
         this.intentosFallidos = 0;
-        this.accesoExitoso = true;
-        return true;
-    } else {
-        this.intentosFallidos++;
-        if (this.intentosFallidos >= this.maxIntentos) {
-            this.bloqueado = true;
+        this.bloqueado = false;
+        this.accesoExitoso = false;
+
+        if (maxIntentos <= 0) {
+            this.maxIntentos = 3;
+        } else {
+            this.maxIntentos = maxIntentos;
         }
-        return false;
-    }
-}
-public void reiniciarAcceso() {
-    this.intentosFallidos = 0;
-    this.bloqueado = false;
-}
-public boolean cambiarPassword(String actual, String nueva) {
-    if (this.bloqueado) {
-        return false;
-    }
-    if (!this.password.equals(actual)) {
-        return false;
     }
 
-    if (!validarPasswordSegura(nueva)) {
-        return false;
+    public String getUsername() {
+        return username;
     }
-    this.password = nueva;
-    return true;
-}
-public boolean validarPasswordSegura(String nueva) {
-    if (nueva == null || nueva.length() < 8) {
-        return false;
-    }
-}
-boolean tieneMayuscula = false;
-boolean tieneNumero = false;
 
-for (int i = 0; i < nueva.length(); i++) {
-char c = nueva.charAt(i);
-if (Character.isUpperCase(c)) {
-tieneMayuscula = true;
-}
-if (Character.isDigit(c)) {
-tieneNumero = true;
-}
-}
-return tieneMayuscula && tieneNumero;
-}
+    public int getIntentosFallidos() {
+        return intentosFallidos;
+    }
+
+    public boolean isBloqueado() {
+        return bloqueado;
+    }
+
+    public int getMaxIntentos() {
+        return maxIntentos;
+    }
+
+    public boolean isAccesoExitoso() {
+        return accesoExitoso;
+    }
+
+    public boolean autenticar(String passwordIngresada) {
+        if (this.bloqueado) {
+            return false;
+        }
+
+        if (this.password.equals(passwordIngresada)) {
+            this.intentosFallidos = 0;
+            this.accesoExitoso = true;
+            return true;
+        } else {
+            this.intentosFallidos++;
+            this.accesoExitoso = false;
+
+            if (this.intentosFallidos >= this.maxIntentos) {
+                this.bloqueado = true;
+            }
+            return false;
+        }
+    }
+
+    public void reiniciarAcceso() {
+        this.intentosFallidos = 0;
+        this.bloqueado = false;
+        this.accesoExitoso = false;
+    }
+
+    public boolean cambiarPassword(String actual, String nueva) {
+        if (this.bloqueado) {
+            return false;
+        }
+
+        if (!this.password.equals(actual)) {
+            return false;
+        }
+
+        if (!validarPasswordSegura(nueva)) {
+            return false;
+        }
+
+        this.password = nueva;
+        return true;
+    }
+
+    public boolean validarPasswordSegura(String nueva) {
+        if (nueva == null || nueva.length() < 8) {
+            return false;
+        }
+
+        boolean tieneMayuscula = false;
+        boolean tieneNumero = false;
+
+        for (int i = 0; i < nueva.length(); i++) {
+            char c = nueva.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                tieneMayuscula = true;
+            }
+
+            if (Character.isDigit(c)) {
+                tieneNumero = true;
+            }
+        }
+
+        return tieneMayuscula && tieneNumero;
+    }
 }
