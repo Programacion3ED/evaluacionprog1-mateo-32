dee618cd471ea11558323138d83b25e95960a749
Laboratorio <laboratorio@udla-ec.int>
2026-04-22 10:55:58 -0500

    Mateo Garzon v1

---
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -0,0 +1,10 @@
+public class UsuarioSeguroAvanzado {
+
+    private String username;
+    private String password;
+    private int intentosFallidos;
+    private boolean bloqueado;
+    private int maxIntentos;
+    private boolean accesoExitoso;
+
+}
