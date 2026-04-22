9c143f0b6f9c03181db8ce1d10e757a02dff70bb
Laboratorio <laboratorio@udla-ec.int>
2026-04-22 11:10:30 -0500

    Mateo Garzon v2

--- src/main/java/UsuarioSeguroAvanzado.java
+++ src/main/java/UsuarioSeguroAvanzado.java
@@ -7,4 +7,28 @@ public class UsuarioSeguroAvanzado {
     private int maxIntentos;
     private boolean accesoExitoso;
 
+    this.username = username;
+    this.password = password;
+    this.intentosFallidos = 0;
+    this.bloqueado = false;
+    this.accesoExitoso = false;
+
+    if (maxIntentos<=0) {
+        this.maxIntentos = 3;
+    }else {
+        this.maxIntentos = maxIntentos;
+
+    }
+
+
+}
+
+public String getUsername () {
+    return username;
+}
+public int getIntentosFallidos () {
+    return intentosFallidos;
+}
+public boolean isBloqueado () {
+    return bloqueado;
 }
